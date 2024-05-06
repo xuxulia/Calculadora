764f8560ee05d5e93b13c0b998fb319b371e5158
Júlia Beatriz De Souza <juliabeatriz.souza0704@gmail.com>
2024-05-06 20:33:04 -0300

    Mudança no no visual do index e adicionado o novo campo de despesas extras em calculadora

--- calculadora.js
+++ calculadora.js
@@ -3,6 +3,7 @@ function calcularGorjeta() {
     let totalConta = parseFloat(document.getElementById('totalConta').value);
     let numeroPessoas = parseInt(document.getElementById('numeroPessoas').value);
     let percentualGorjeta = parseFloat(document.getElementById('percentualGorjeta').value);
+    let despesas = parseFloat(document.getElementById('despesas').value);
 
     // vai verificar se os valores inseridos são válidos (achei melhor colocar ne)
     if (isNaN(totalConta) || isNaN(numeroPessoas) || isNaN(percentualGorjeta) || totalConta <= 0 || numeroPessoas <= 0) {
@@ -17,6 +18,8 @@ function calcularGorjeta() {
     // mostrar a gorjeta e o total a pagar por pessoa
     let gorjetaPorPessoa = gorjeta / numeroPessoas;
     let totalPorPessoa = totalPagar / numeroPessoas;
+    let despesasPessoa = despesas / numeroPessoas;
+    totalPorPessoa = totalPorPessoa + despesasPessoa;
 
     // mostrar os resultados na tela (usei o toFixed pra deixar apenas 2 casas decimais)
     document.getElementById('resultadoGorjeta').innerText = 'Gorjeta por pessoa: R$' + gorjetaPorPessoa.toFixed(2);
